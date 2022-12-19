cb070d5ac04350069cb262afd6cee8da92b7b3e0
Nicole <ngott30@gmail.com>
2022-12-19 16:56:39 +0100

    added top most left pixel as reference point for all other pixels

--- VGA_Pikachu.srcs/sources_1/new/vga.v
+++ VGA_Pikachu.srcs/sources_1/new/vga.v
@@ -39,77 +39,85 @@ module vga(
     ///////// Display and produce different colors /////////
     wire display_ok, red_ok, blue_ok, green_ok, yellow_ok, white_ok;
     
+    //reference pixel (is top most left pixel of Pikachu) 
+    reg zeile = 9'd120; //9 Bit to display maximum needed number 340
+    reg pixel = 9'd250; //9 Bit to display maximum needed number 500
+    
+    //Reg fuer Verschiebung nach rechts und links (einfach zu zeile hinzu addieren)
+    
     //display_ok = visible area
     assign display_ok = (zeilencounter >= 45) && (pixelcounter > 160); //Pixel == Columns
     
     //yellow_ok has to be assigned to red and green (are the colors that are needed to generate yellow color)
-    assign yellow_ok = ((zeilencounter >= 120) && (zeilencounter <= 160) 
-            && (((pixelcounter >= 250) && (pixelcounter <= 270)) 
-            || (pixelcounter >= 510) && (pixelcounter <= 530))) //Area 1
-        || ((zeilencounter >= 130) && (zeilencounter <= 170) 
-            && (((pixelcounter >= 270) && (pixelcounter <= 290)) 
-            || (pixelcounter >= 490) && (pixelcounter <= 510))) //Area 2
-        || ((zeilencounter >= 150) && (zeilencounter <= 170) 
-            && (((pixelcounter >= 290) && (pixelcounter <= 310)) 
-            || (pixelcounter >= 470) && (pixelcounter <= 490))) //Area 3
-        || ((zeilencounter >= 160) && (zeilencounter <= 170) 
-            && (((pixelcounter >= 270) && (pixelcounter <= 350)) 
-            || ((pixelcounter >= 370) && (pixelcounter <= 410)) 
-            || ((pixelcounter >= 430) && (pixelcounter <= 510)))) //Area 4
-        || ((zeilencounter >= 170) && (zeilencounter <= 190) 
-            && ((pixelcounter >= 270) && (pixelcounter <= 510))) //Area 5
+    assign yellow_ok = ((zeilencounter >= zeile) && (zeilencounter <= (zeile+20))) 
+            && (((pixelcounter >= pixel && (pixelcounter <= (pixel+20))) 
+            || (pixelcounter >= (pixel+260)) && (pixelcounter <= (pixel+280)))) //Area 1
+        || ((zeilencounter >= (zeile+10) && (zeilencounter <= (zeile+50)) 
+            && (((pixelcounter >= (pixel+20)) && (pixelcounter <= (pixel+40))) 
+            || (pixelcounter >= (pixel+240)) && (pixelcounter <= (pixel+260)))) //Area 2
+        || ((zeilencounter >= (zeile+30)) && (zeilencounter <= (zeile+50)) 
+            && (((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+60))) 
+            || (pixelcounter >= (pixel+220)) && (pixelcounter <= (pixel+270)))) //Area 3
+        || ((zeilencounter >= (zeile+40)) && (zeilencounter <= (zeile+50)) 
+            && (((pixelcounter >= (pixel+20)) && (pixelcounter <= (pixel+100))) 
+            || ((pixelcounter >= (pixel+120)) && (pixelcounter <= (pixel+160))) 
+            || ((pixelcounter >= (pixel+180)) && (pixelcounter <= (pixel+260))))) //Area 4
+        || ((zeilencounter >= (zeile+50)) && (zeilencounter <= (zeile+70)) 
+            && ((pixelcounter >= (pixel+20)) && (pixelcounter <= (pixel+260)))) //Area 5
         || ((zeilencounter >= 190) && (zeilencounter <= 210) 
-            && ((pixelcounter >= 290) && (pixelcounter <= 490))) //Area 6
-        || ((zeilencounter >= 210) && (zeilencounter <= 230) 
-            && ((pixelcounter >= 300) && (pixelcounter <= 480))) //Area 12
-        || ((zeilencounter >= 230) && (zeilencounter <= 240) 
-            && (((pixelcounter >= 290) && (pixelcounter <= 330)) 
-            || ((pixelcounter >= 350) && (pixelcounter <= 430)) 
-            || ((pixelcounter >= 450) && (pixelcounter <= 490)))) //Area 13
-        || ((zeilencounter >= 240) && (zeilencounter <= 260) 
-            && (((pixelcounter >= 290) && (pixelcounter <= 320))
-            || ((pixelcounter >= 360) && (pixelcounter <= 420))
-            || ((pixelcounter >= 460) && (pixelcounter <= 490)))) //Area 14
-        || ((zeilencounter >= 260) && (zeilencounter <= 270) 
-            && (((pixelcounter >= 290) && (pixelcounter <= 330)) 
-            || ((pixelcounter >= 350) && (pixelcounter <= 380)) 
-            || ((pixelcounter >= 400) && (pixelcounter <= 430)) 
-            || ((pixelcounter >= 450) && (pixelcounter <= 490)))) //Area 15
-        || ((zeilencounter >= 270) && (zeilencounter <= 280) 
-            && (((pixelcounter >= 280) && (pixelcounter <= 310)) 
-            || ((pixelcounter >= 330) && (pixelcounter <= 450)) 
-            || ((pixelcounter >= 470) && (pixelcounter <= 500)))) //Area 16
-        || ((zeilencounter >= 280) && (zeilencounter <= 290) 
-            && (((pixelcounter >= 280) && (pixelcounter <= 300)) 
-            || ((pixelcounter >= 340) && (pixelcounter <= 360)) 
-            || ((pixelcounter >= 370) && (pixelcounter <= 380)) 
-            || ((pixelcounter >= 400) && (pixelcounter <= 410)) 
-            || ((pixelcounter >= 420) && (pixelcounter <= 440)) 
-            || ((pixelcounter >= 480) && (pixelcounter <= 500)))) //Area 17
-        || ((zeilencounter >= 290) && (zeilencounter <= 300) 
-            && (((pixelcounter >= 280) && (pixelcounter <= 300)) 
-            || ((pixelcounter >= 340) && (pixelcounter <= 370)) 
-            || ((pixelcounter >= 410) && (pixelcounter <= 440)) 
-            || ((pixelcounter >= 480) && (pixelcounter <= 500)))) //Area 18
-        || ((zeilencounter >= 300) && (zeilencounter <= 310) 
-            && (((pixelcounter >= 290) && (pixelcounter <= 310)) 
-            || ((pixelcounter >= 330) && (pixelcounter <= 370)) 
-            || ((pixelcounter >= 410) && (pixelcounter <= 450)) 
-            || ((pixelcounter >= 470) && (pixelcounter <= 490)))) //Area 19
-        || ((zeilencounter >= 310) && (zeilencounter <= 320) 
-            && (((pixelcounter >= 300) && (pixelcounter <= 370)) 
-            || ((pixelcounter >= 410) && (pixelcounter <= 480)))) //Area 20
-        || ((zeilencounter >= 320) && (zeilencounter <= 330) 
-            && (((pixelcounter >= 310) && (pixelcounter <= 380)) 
-            || ((pixelcounter >= 400) && (pixelcounter <= 470)))) //Area 21
-        || ((zeilencounter >= 330) && (zeilencounter <= 340) 
-            && (((pixelcounter >= 330) && (pixelcounter <= 450)))); //Area 22
+            && ((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+240)))) //Area 6
+        || ((zeilencounter >= (zeile+90)) && (zeilencounter <= (zeile+110)) 
+            && ((pixelcounter >= (pixel+60)) && (pixelcounter <= (pixel+230)))) //Area 12
+        || ((zeilencounter >= (zeile+110)) && (zeilencounter <= (zeile+120)) 
+            && (((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+80))) 
+            || ((pixelcounter >= (pixel+100)) && (pixelcounter <= (pixel+180))) 
+            || ((pixelcounter >= (pixel+200)) && (pixelcounter <= (pixel+240))))) //Area 13
+        || ((zeilencounter >= (zeile+120)) && (zeilencounter <= (zeile+140)) 
+            && (((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+70)))
+            || ((pixelcounter >= (pixel+110)) && (pixelcounter <= (pixel+170)))
+            || ((pixelcounter >= (pixel+210)) && (pixelcounter <= (pixel+240))))) //Area 14
+        || ((zeilencounter >= (zeile+140)) && (zeilencounter <= (zeile+150)) 
+            && (((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+80))) 
+            || ((pixelcounter >= (pixel+100)) && (pixelcounter <= (pixel+130))) 
+            || ((pixelcounter >= (pixel+150)) && (pixelcounter <= (pixel+180))) 
+            || ((pixelcounter >= (pixel+200)) && (pixelcounter <= (pixel+240))))) //Area 15
+        || ((zeilencounter >= (zeile+150)) && (zeilencounter <= (zeile+160)) 
+            && (((pixelcounter >= (pixel+30)) && (pixelcounter <= (pixel+60))) 
+            || ((pixelcounter >= (pixel+80)) && (pixelcounter <= (pixel+200))) 
+            || ((pixelcounter >= (pixel+220)) && (pixelcounter <= (pixel+250))))) //Area 16
+        || ((zeilencounter >= (zeile+160)) && (zeilencounter <= (zeile+170)) 
+            && (((pixelcounter >= (pixel+30)) && (pixelcounter <= (pixel+250))) 
+            || ((pixelcounter >= (pixel+90)) && (pixelcounter <= (pixel+110))) 
+            || ((pixelcounter >= (pixel+120)) && (pixelcounter <= (pixel+130))) 
+            || ((pixelcounter >= (pixel+150)) && (pixelcounter <= (pixel+160))) 
+            || ((pixelcounter >= (pixel+170)) && (pixelcounter <= (pixel+190))) 
+            || ((pixelcounter >= (pixel+230)) && (pixelcounter <= (pixel+250))))) //Area 17
+        || ((zeilencounter >= (zeile+170)) && (zeilencounter <= (zeile+180)) 
+            && (((pixelcounter >= (pixel+30)) && (pixelcounter <= (pixel+50))) 
+            || ((pixelcounter >= (pixel+90)) && (pixelcounter <= (pixel+120))) 
+            || ((pixelcounter >= (pixel+160)) && (pixelcounter <= (pixel+190))) 
+            || ((pixelcounter >= (pixel+230)) && (pixelcounter <= (pixel+250))))) //Area 18
+        || ((zeilencounter >= (zeile+180)) && (zeilencounter <= (zeile+190)) 
+            && (((pixelcounter >= (pixel+40)) && (pixelcounter <= (pixel+60))) 
+            || ((pixelcounter >= (pixel+80)) && (pixelcounter <= (pixel+120))) 
+            || ((pixelcounter >= (pixel+160)) && (pixelcounter <= (pixel+200))) 
+            || ((pixelcounter >= (pixel+220)) && (pixelcounter <= (pixel+240))))) //Area 19
+        || ((zeilencounter >= (zeile+190)) && (zeilencounter <= (zeile+200)) 
+            && (((pixelcounter >= (pixel+50)) && (pixelcounter <= (pixel+120))) 
+            || ((pixelcounter >= (pixel+160)) && (pixelcounter <= (pixel+230))))) //Area 20
+        || ((zeilencounter >= (zeile+200)) && (zeilencounter <= (zeile+210)) 
+            && (((pixelcounter >= (pixel+60)) && (pixelcounter <= (pixel+130))) 
+            || ((pixelcounter >= (pixel+150)) && (pixelcounter <= (pixel+220))))) //Area 21
+        || ((zeilencounter >= (zeile+210)) && (zeilencounter <= (zeile+220)) 
+            && (((pixelcounter >= (pixel+80)) && (pixelcounter <= (pixel+200)))))); //Area 22
     
     //white_ok has to be assigned to red, green and blue (are the colors that are needed to generate white color)
+    //white for Pikachu's eyes
     assign white_ok = ((zeilencounter >= 240) && (zeilencounter <= 250) 
             && (((pixelcounter >= 340) && (pixelcounter <= 350))
-            || ((pixelcounter >= 440) && (pixelcounter <= 450)))); //Eyes of Pikachu
+            || ((pixelcounter >= 440) && (pixelcounter <= 450))));
     
+    //red for Pikachu's cheeks
     assign red_ok = ((zeilencounter >= 270) && (zeilencounter <= 280) 
             && (((pixelcounter >= 310) && (pixelcounter <= 330)) 
             || ((pixelcounter >= 450) && (pixelcounter <= 470)))) //Red Area of Area 16
